20c7491a91cb71537e515ccaa57e26e729e2365d
0xKanu <kanuonyedikachi7@gmail.com>
2022-06-03 02:13:40 +0000

    need to learn betty

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,8 +1,9 @@
+/*COde to print with the puts() function*/
 #include <stdio.h>
 
 /**
- * main prints \"Programming is like building a multilingual puzzle
- * return 0 if true 
+ * main - prints \"Programming is like building a multilingual puzzle
+ * return if true 
  **/
 int main(void)
 {
@@ -10,5 +11,5 @@ int main(void)
 	char Mystr[] = "\"Programming is like building a multilingual puzzle";
 
 	puts(Mystr);
-return (0);
+return (0); /*return 0 of true*/
 }
