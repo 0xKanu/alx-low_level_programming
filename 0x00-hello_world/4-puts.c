e8056f867aa9c1b6cfb6b0dff3301cd920ad2891
0xKanu <kanuonyedikachi7@gmail.com>
2022-06-03 02:16:05 +0000

    need to learn betty

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -2,9 +2,9 @@
 #include <stdio.h>
 
 /**
- * main - prints \"Programming is like building a multilingual puzzle
- * return if true 
- **/
+*main - prints \"Programming is like building a multilingual puzzle
+*return(0), if true 
+**/
 int main(void)
 {
 	/*string initialisation*/
