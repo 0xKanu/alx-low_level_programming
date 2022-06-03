423a7c09d3a50baaa695c81132b3197571a6286c
0xKanu <kanuonyedikachi7@gmail.com>
2022-06-03 02:32:15 +0000

    0x00 Task 5

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,12 @@
+/*COde to print with the printf() function*/
+#include <stdio.h>
+
+/**
+ *main - prints "with proper grammar, but the outcome is a piece of art,"
+ *Return: 0 if the number is positive. non-zero otherwise
+ **/
+int main(void)
+{
+printf("with proper grammar, but the outcome is a piece of art,","%s/n");
+return (0); /*return 0 of true*/
+}
