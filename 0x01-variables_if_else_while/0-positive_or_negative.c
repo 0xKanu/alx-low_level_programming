a72c1e70ed75f639ba77882b96c34c02fdd0ad94
0xKanu <kanuonyedikachi7@gmail.com>
2022-06-06 09:01:11 +0000

    Task 0 betty corrections.

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -3,9 +3,10 @@
 #include <time.h>
 /* more headers goes there */
 
-/** main - This function tells if a random number
+/**
+ * main - This function tells if a random number
  * generated is a positive, negative or zero.
- * Return : O if succesful, otherwise it is not. 
+ * Return: O if successful, otherwise it is not.
  */
 int main(void)
 {
@@ -16,15 +17,15 @@ int main(void)
 
 	if (n > 0)
 	{
-		printf("%d is positive\n", n); 
-	}	
+		printf("%d is positive\n", n);
+	}
 	else if (n == 0)
 	{
-		printf("%d is zero\n", n); 
+		printf("%d is zero\n", n);
 	}
 	else
 	{
-		printf("%d is negative\n", n); 
-	}	
+		printf("%d is negative\n", n);
+	}
 	return (0);
 }
