0c4f6d45209fd41f22543a702cfd824701d9e2b5
0xKanu <kanuonyedikachi7@gmail.com>
2022-06-06 10:03:11 +0000

    Task 4 betty corrections.

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -1,17 +1,21 @@
 #include <stdio.h>
-
+/**
+  * main - prints alphabets execpt 'q' & 'e'.
+  * Return: 0 if successful. 
+  */
 int main(void)
 {
-char beta = 97;
+	char beta = 97;
 
-while (beta <= 122)
-{
-if ((beta!=101) && (beta!=113))
-{putchar(beta);}
-beta ++;
+	while (beta <= 122)
+	{
+		if ((beta!=101) && (beta!=113))
+		{putchar(beta); }
+
+	beta++;
+	}
+
+	putchar(10);
+
+return (0);
 }
-
-putchar(10);
-
-return(0);
-}	
