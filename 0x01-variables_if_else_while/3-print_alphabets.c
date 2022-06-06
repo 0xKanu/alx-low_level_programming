ad2c04fa74331fe09f6a49ce059c4869994648bc
0xKanu <kanuonyedikachi7@gmail.com>
2022-06-06 09:48:25 +0000

    Task 3 betty corrections.

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -1,22 +1,27 @@
 #include <stdio.h>
 
+/**
+  * main - this function prints the lowercase alphabets first then 
+  * UPPERCASE letters.
+  * Return: 0 always if successful.
+  */
 int main(void)
 {
 	char alpha = 65;
 	char beta = 97;
 
-while(beta <= 122 )
+while (beta <= 122)
 {
 putchar(beta);
 beta ++;
 }
-while(alpha <= 90) 
+while (alpha <= 90)
 {
 putchar(alpha);
 alpha ++;
 }
 putchar(10);
 
-return(0);
+return (0);
 }
 
