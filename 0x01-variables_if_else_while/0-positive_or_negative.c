fcfbabbbdef910722e2474d2ab42cb290585a1ea
0xKanu <kanuonyedikachi7@gmail.com>
2022-06-06 08:49:15 +0000

    Task 0 betty corrections.

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -3,9 +3,9 @@
 #include <time.h>
 /* more headers goes there */
 
-/* main - This function tells if a random number
- *  generated is a positive, negative or zero.
- * Return : O if succesful, otherwise it is not. 
+/** main - This function tells if a random number
+ *generated is a positive, negative or zero.
+ *Return : O if succesful, otherwise it is not. 
  */
 int main(void)
 {
@@ -15,14 +15,10 @@ int main(void)
 	n = rand() - RAND_MAX / 2;
 
 	if (n > 0)
-	{
-	printf("%d is positive\n", n);}	
+	{printf("%d is positive\n", n);}	
 	else if (n == 0)
-	{
-	printf("%d is zero\n", n);
-	}
+	{printf("%d is zero\n", n);}
 	else
-	{
-	printf("%d is negative\n", n);}	
+	{printf("%d is negative\n", n);}	
 	return (0);
 }
