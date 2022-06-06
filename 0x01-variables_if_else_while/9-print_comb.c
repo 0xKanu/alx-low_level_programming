305325ec9761da66ae37e28360d918faa7745dd6
0xKanu <kanuonyedikachi7@gmail.com>
2022-06-06 11:56:49 +0000

    Task 9 betty corrections.

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -1,27 +1,29 @@
 #include <stdio.h>
 
 /**
-  *
+  * main - function prints all possible combinations of single-digit numbers.
+  * Return: 0 if successful.
   */
 
 int main(void)
 {
 	int n = 48;
-	
+
 	while(n <= 57)
-	{	
+	{
 		putchar(n);
-                
+
 		if (n < 57)
+		{
 		putchar(44);
 
 		putchar(' ');
+		}
 
 		n++;
 
 	}
 
 	putchar(10);
-return (0);	
+return (0);
 }
-
