d57f364476d11e46883e86f6161d65c3e5ca6b10
0xKanu <kanuonyedikachi7@gmail.com>
2022-06-06 10:40:05 +0000

    Task 6 betty corrections.

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -1,5 +1,9 @@
 #include <stdio.h>
-
+/**
+  * main -  prints all single digit numbers of base 10 starting from 0
+  * , followed by a new line.
+  * Return: 0 if successful.
+  */
 int main(void)
 {
 	char number = 48;
