810c920abfd98cc44db33820cb9296ac472399b8
0xKanu <kanuonyedikachi7@gmail.com>
2022-06-09 01:59:10 +0000

    Task 2 completed and manually checked

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+  * print_alphabet_x10 - print the alphabet x 10
+  */
+void print_alphabet_x10(void)
+	{
+		int n;
+		int times;
+
+		for(times = 0; times <= 10; times ++)
+		{	
+		for (n = 97; n <= 122; n++)
+			_putchar(n);
+
+		_putchar(10);
+		}
+	return;
+	}
