78c5d6d8a0dcfd8a08464f6de5c59317f10c36cc
0xKanu <kanuonyedikachi7@gmail.com>
2022-06-09 01:41:28 +0000

    betty o betty

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,13 @@
+/*Code to print with the putchar() function*/
+#include <stdio.h>
+
+/**
+  *main - prints "and that piece of art is useful" - Dora Korpar, 2015-10-19"
+  *Return: 1 if the code runs smoothly. non-zero otherwise
+  **/
+int main(void)
+{
+char Mystr[] = "\"and that piece of art is useful\" - Dora Korpar, 2015-10-19\"";
+char putchar(Mystr);
+return (1);
+}
