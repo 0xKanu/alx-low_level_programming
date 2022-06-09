1d552970db55f687394733c72be8bc899c8013b7
0xKanu <kanuonyedikachi7@gmail.com>
2022-06-09 11:27:52 +0000

    This has happened, never make a mistake twice.

--- 0x03-debugging/2-largest_number.c
+++ 0x03-debugging/2-largest_number.c
@@ -11,19 +11,20 @@
 int largest_number(int a, int b, int c)
 {
 	int largest;
-
-        if (a > b && b > c)
+	
+	if (a > b)
 	{
-		largest = a;
-        }
-	if (b > a && a > c)
-        {
-		largest = b;
+		if (a > c)
+			largest = a;
+		else 
+			largest = c;
 	}
-	if (c >= a && c >= b)
+	else
 	{
-		largest = c;
+		if (b > c)
+			largest = b;
+		else
+			largest = c;
 	}
-
 return (largest);
 }
