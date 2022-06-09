fe560b91b5a20c6de22624898e4866b975f99180
0xKanu <kanuonyedikachi7@gmail.com>
2022-06-09 02:05:55 +0000

    Task 2 completed and manually checked

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -7,12 +7,12 @@ void print_alphabet_x10(void)
 		int n;
 		int times;
 
-		for (times = 0; times <= 10; times++)
+		for (times = 1; times <= 10; times++)
 		{
 		for (n = 97; n <= 122; n++)
 			_putchar(n);
-		}
 
 		_putchar(10);
+		}
 	return;
 	}
