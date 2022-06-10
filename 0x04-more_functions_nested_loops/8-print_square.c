86f44457578d2356b14ba6a7a1fe67ed78898340
0xKanu <kanuonyedikachi7@gmail.com>
2022-06-10 02:52:49 +0000

    complete?

--- 0x04-more_functions_nested_loops/8-print_square.c
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -8,11 +8,22 @@
   */
 void print_square(int size);
 {
+	int rows = size;
+	int orSize = size;
+
 	if(size > 0)
 	{
-	
+	while (size--)
+	{
+		while (rows--)
+		{
+			_putchar(35);
+		}
+		_putchar(10);
+		rows = orSize;
+	}
 	}
 	else
-	putchar(10);
+	_putchar(10);
 return;	
 }
