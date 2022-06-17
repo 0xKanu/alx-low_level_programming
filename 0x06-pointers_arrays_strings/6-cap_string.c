61d90b59712226fcf321b93543147d1276d54585
0xKanu <kanuonyedikachi7@gmail.com>
2022-06-17 02:02:09 +0000

    Task 6 complete, first check

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -1,3 +1,29 @@
 #include "main.h"
 /**
   * cap_string - a function that capatilizes every word of a string.
+  * @ s: the string to be modified.
+  *
+  * Return: the resulting string.
+  */
+char *cap_string(char *s)
+{
+	int x = 0;
+	int y;
+	char special[13] = {9, 10, 32, 33, 34, 40, 41, 44, 46, 59, 63, 123, 125}
+
+	while (*(s + x))
+	{
+		for (y = 0; y < 13, y++)
+		{
+			if  (x == 0 && s[x] >= 97 && s[x] <= 122)
+				s[x] -= 32;
+			if (s[x - 1] == special[y])
+			{
+				if ((*(s + x) >= 97) && (*(s + x) <= 122))
+					*(s + x) -= 32;
+			}
+		}
+		x++;
+	}
+	return (s);
+}
