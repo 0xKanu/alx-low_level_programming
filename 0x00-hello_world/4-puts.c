db519b9fda99a6c62996b7a7ccd75f7f2f865d03
0xKanu <kanuonyedikachi7@gmail.com>
2023-09-14 06:35:06 +0000

    Create and complete task 4; Hello, puts

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+ * main - entry point to program
+ *
+ * Return: void
+ */
+
+int main(void)
+{
+	puts("Programming is like building a multilingual puzzle\n");
+
+	return (0);
+}
+
+
