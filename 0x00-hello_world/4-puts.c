8becb2fe578a22ce87dab87c0e088bc954a6b98f
0xKanu <kanuonyedikachi7@gmail.com>
2023-09-14 06:41:21 +0000

    Create and complete task 4; Hello, puts#2

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -5,9 +5,9 @@
  * Return: void
  */
 
-int main()
+int main(void)
 {
-	puts("Programming is like building a multilingual puzzle\n");
+	puts("Programming is like building a multilingual puzzle");
 
 	return (0);
 }
