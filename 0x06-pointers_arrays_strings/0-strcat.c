7b6b7e69b79dd08f86fd6ef5a6ddaec8f3c2c2e3
0xKanu <kanuonyedikachi7@gmail.com>
2023-09-22 02:55:04 +0000

    Correct typos in task 1, main header file

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -27,7 +27,7 @@ char *_strcat(char *dest, char *src)
 		x++;
 	}
 
-	des[j] = '\0';
+	dest[x] = '\0';
 
 
 
--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -2,15 +2,15 @@
 #define MAIN_FILE
 
 int _putchar(char c);
-char *_strcat(chat *dest, char *src);
+char *_strcat(char *dest, char *src);
 char *_strncat(char *dest, char *src, int n);
-char *_strncpy(cahr *dest, char *src, int n);
+char *_strncpy(char *dest, char *src, int n);
 int _strcmp(char *s1, char *s2);
 void revers_array(int *a, int n);
 char *string_toupper(char *);
 char *cap_string(char *);
 char *leet(char *);
 void print_number(int n);
-char *infinite_add(char *n1, char *n2, char *r int size_r);
+char *infinite_add(char *n1, char *n2, char *r, int size_r);
 
 #endif
