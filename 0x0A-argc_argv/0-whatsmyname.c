4d85072edd7b66d0b8b177fd68c71853b6a6dfae
0xKanu <kanuonyedikachi7@gmail.com>
2024-01-18 01:18:08 +0100

    add task 0 to project argc and argv

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,19 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - print its name, followed by a newline
+ * @argc: int pointer param
+ * @argv: char array
+ *
+ * Return: void.
+ */
+
+int main(int argc, char **argv)
+{
+	printf("%s\n", *argv);
+
+	exit(EXIT_SUCCESS);
+
+}
+
