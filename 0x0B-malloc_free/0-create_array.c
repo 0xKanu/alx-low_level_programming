38db355417e59c90eeb6b8391599e29cfea79b6a
0xKanu <kanuonyedikachi7@gmail.com>
2024-01-18 23:21:19 +0100

    more corrections of task 0.2

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -15,7 +15,7 @@ char *create_array(unsigned int size, char c)
 	char *str;
 	unsigned int count;
 
-	str = malloc(sizeof(char) * size);
+	str = (char *) malloc(sizeof(char) * size);
 
 	if (str == NULL || size == 0)
 		return (NULL);
