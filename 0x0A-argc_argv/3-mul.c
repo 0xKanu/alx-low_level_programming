1dc126183fe12406b43b5dff4a1e410c944704d3
0xKanu <kanuonyedikachi7@gmail.com>
2024-01-18 02:16:00 +0100

    submit tested tasl 3 for checker

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,26 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - multiplies two nums,
+ * followed by newline.
+ * @argc: int pointer param
+ * @argv: char array
+ *
+ * Return: 0 if successful, 1 if unsuccessful
+ */
+
+int main(int argc, char *argv[])
+{
+	if (argc != 3)
+	{
+		printf("Error\n");
+		return(1);
+	}
+	
+	printf("%d\n", atoi(argv[1]) * atoi(argv[2]));
+
+	return (0);
+
+}
+
