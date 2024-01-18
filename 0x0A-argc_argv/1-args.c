db2663a51b02912cd18791bf622be3328d938c9b
0xKanu <kanuonyedikachi7@gmail.com>
2024-01-18 01:41:00 +0100

    add complete task 1 for first check

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,20 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - print the number of args passed into it.
+ * @argc: int pointer param
+ * @argv: char array
+ *
+ * Return: void.
+ */
+
+int main(int argc, char **argv)
+{
+	(void)argv;
+	printf("%d\n", argc);
+
+	exit(EXIT_SUCCESS);
+
+}
+
